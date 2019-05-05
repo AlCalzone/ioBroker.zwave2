3316c449b1945a96d91591562fb0b70acbb4ae9c
Dominic Griesel <d.griesel@gmx.net>
2019-05-05 15:11:15 +0200

    "Initial commit"

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"zwave2 adapter settings": {
+		"en": "Adapter settings for zwave2",
+		"de": "Adaptereinstellungen für zwave2",
+		"ru": "Настройки адаптера для zwave2",
+		"pt": "Configurações do adaptador para zwave2",
+		"nl": "Adapterinstellingen voor zwave2",
+		"fr": "Paramètres d'adaptateur pour zwave2",
+		"it": "Impostazioni dell'adattatore per zwave2",
+		"es": "Ajustes del adaptador para zwave2",
+		"pl": "Ustawienia adaptera dla zwave2",
+		"zh-cn": "zwave2的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
---
+++ build/lib/tools.js
@@ -0,0 +1,57 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = require("axios");
+/**
+ * Tests whether the given variable is a real object and not an Array
+ * @param it The variable to test
+ */
+function isObject(it) {
+    // This is necessary because:
+    // typeof null === 'object'
+    // typeof [] === 'object'
+    // [] instanceof Object === true
+    return Object.prototype.toString.call(it) === "[object Object]";
+}
+exports.isObject = isObject;
+/**
+ * Tests whether the given variable is really an Array
+ * @param it The variable to test
+ */
+function isArray(it) {
+    if (Array.isArray != null)
+        return Array.isArray(it);
+    return Object.prototype.toString.call(it) === "[object Array]";
+}
+exports.isArray = isArray;
+/**
+ * Translates text using the Google Translate API
+ * @param text The text to translate
+ * @param targetLang The target languate
+ */
+function translateText(text, targetLang) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (targetLang === "en")
+            return text;
+        try {
+            const url = `http://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=${targetLang}&dt=t&q=${encodeURIComponent(text)}&ie=UTF-8&oe=UTF-8`;
+            const response = yield axios_1.default({ url, timeout: 5000 });
+            if (isArray(response.data)) {
+                // we got a valid response
+                return response.data[0][0][0];
+            }
+            throw new Error("Invalid response for translate request");
+        }
+        catch (e) {
+            throw new Error(`Could not translate to "${targetLang}": ${e}`);
+        }
+    });
+}
+exports.translateText = translateText;
