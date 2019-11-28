922a0f9514a7cea0836a4362b87619c6f90721ae
dominic.griesel <d.griesel@gmx.net>
2019-11-28 12:52:18 +0100

    chore: add jest for local testing

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	testEnvironment: "node",
+	roots: ["<rootDir>/src"],
+	testRegex: "(\\.|/)test\\.tsx?$",
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	setupFilesAfterEnv: ["jest-extended"],
+	setupFiles: ["reflect-metadata"],
+	extraGlobals: ["Reflect"],
+	collectCoverage: false,
+	collectCoverageFrom: ["src/**/*.ts", "!src/**/*.test.ts"],
+	coverageReporters: ["lcov", "html", "text-summary"],
+};
